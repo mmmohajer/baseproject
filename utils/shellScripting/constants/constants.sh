4246abac4d6bbd017f2904733ef1b22b4683b9e4
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2024-06-06 01:11:15 -0400

    Docker Swarm added

--- utils/shellScripting/constants/constants.sh
+++ utils/shellScripting/constants/constants.sh
@@ -1,5 +1,5 @@
 BOLD_TXT=$(tput bold)
 NORMAL_TXT=$(tput sgr0)
 OPTIONS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 Q)
-STAGING_SERVER_ALIAS=IswadStaging
-PROD_SERVER_ALIAS=IswadProd
+STAGING_SERVER_ALIAS=CompleteTestSwarm
+PROD_SERVER_ALIAS=CompleteTestSwarm
